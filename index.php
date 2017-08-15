322d2aea5a9929a12ad47527a296a0fba74baa84
sushil50889 <mandi.sushil@gmail.com>
2017-08-15 22:05:43 +0530

    styled ver. 1.0
    css added to the form.

--- index.php
+++ index.php
@@ -9,22 +9,25 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./css/style.css">
   <title>Image Upload</title>
 </head>
 <body>
-    <h1>Upload a File</h1>
+   <div>
+		<h1>Upload a File</h1>
 
-    <form action="uploadProcess.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="image"/>
-        <input type="submit" name="submit"/>
-    </form>
+		<form action="uploadProcess.php" method="post" enctype="multipart/form-data">
+			<input type="file" name="image"/>
+			<input type="submit" name="submit" id="button"/>
+		</form>
 
-    <p>
-        <?php
-            echo $_SESSION["msg"];
-            $_SESSION["msg"] = '';
-        ?>
-    </p>
+		<p>
+			<?php
+				echo $_SESSION["msg"];
+				$_SESSION["msg"] = '';
+			?>
+		</p>
+    </div>
 
 </body>
 </html>
